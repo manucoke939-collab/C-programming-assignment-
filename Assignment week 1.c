1ee4e67256f655cb2c62de97ea26ac246739417a
manucoke939-collab <manucoke939@gmail.com>
2026-09-20 16:53:14 +0300

    Add files via upload

---
+++ Assignment week 1.c
@@ -0,0 +1,13 @@
+//Emmanuel Murimi 
+//CT100/G/30744/26
+#include <stdio.h>
+int main()
+{
+    printf("Unit Offered This semester:\n");
+    printf("1. Introduction to programming\n");
+    printf("2. Data structures\n");
+    printf("3. Computer Networks\n");
+    printf("4. Database systems \n");
+    printf("5. Software Engineering\n");
+    return 0;
+}
